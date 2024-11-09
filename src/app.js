3a50c2a7453382922d4c1385f55608b819aee504
MafeGalvis7 <mariafernandagalvis33@hotmail.com>
2024-11-09 22:02:00 +0000

    First Commit

--- src/app.js
+++ src/app.js
@@ -9,3 +9,31 @@ window.onload = function() {
   //write your code here
   console.log("Hello Rigo from the console!");
 };
+let who=[The dog", "My grandma", "His turtle", "My bird];
+let action = ["ate", "peed", "crushed","broke" ];
+let what = ["my homework", "the keys", "the car"];
+let when= [
+  "before the class",
+  "right on time",
+  "when I finished",
+  "during my lunch",
+  "while I was praying",
+];
+
+const getRandomWlementElement = listElements => {
+  let element= "";
+  element = listElements[Math.floor(Math.ramdom() * listElements.Legenth)]
+return Element;
+}
+
+let excuseGen = document.querySelector("excuse");
+excuseGen.indexhtml = getRandomWlementElement(who) +
+"" + getRandomWlementElement(action) + 
+"" + getRandomWlementElement(what) + 
+"" + getRandomWlementElement(when))
+
+console.log(
+  getRandomWlementElement(who) +
+ "" + getRandomWlementElement(action) + 
+ "" + getRandomWlementElement(what) + 
+ "" + getRandomWlementElement(when))
